80e1a158a322b9b7c8f706821f44dcafbb41b8ab
Ozan Ulutaş <oznulutas@gmail.com>
2023-01-30 09:00:24 +0300

    feat: create users list view

--- src/features/Explore/Users/UsersList/UsersList.tsx
+++ src/features/Explore/Users/UsersList/UsersList.tsx
@@ -1,5 +1,38 @@
+import { useSelector } from "react-redux";
+
+import { selectUsers } from "src/features/Explore/selectors";
+
+import {
+  Avatar,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+import { Fragment } from "react";
+
 function UsersList() {
-  return <div>UsersList</div>;
+  const users = useSelector(selectUsers);
+
+  return (
+    <List>
+      {users.map(({ id, categories, username }) => (
+        <Fragment key={id}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar>OU</Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={username}
+              secondary={categories.map((category) => category).join()}
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </Fragment>
+      ))}
+    </List>
+  );
 }
 
 export default UsersList;
